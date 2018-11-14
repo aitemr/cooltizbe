1b58850f2b62c14b7196e3be605989fcb8687b1a
Islam Temirbek <aitemr16@gmail.com>
2018-11-15 00:17:10 +0600

    feat: add icon & core controllers

--- cooltizbe/cooltizbe/ScheduleViewController.swift
+++ cooltizbe/cooltizbe/ScheduleViewController.swift
@@ -1,30 +1,8 @@
-//
-//  ScheduleViewController.swift
-//  cooltizbe
-//
-//  Created by Islam Temirbek on 11/15/18.
-//  Copyright © 2018 Islam Temirbek. All rights reserved.
-//
-
 import UIKit
 
 class ScheduleViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- cooltizbe/cooltizbe/SearchViewController.swift
+++ cooltizbe/cooltizbe/SearchViewController.swift
@@ -1,30 +1,8 @@
-//
-//  SearchViewController.swift
-//  cooltizbe
-//
-//  Created by Islam Temirbek on 11/15/18.
-//  Copyright © 2018 Islam Temirbek. All rights reserved.
-//
-
 import UIKit
 
 class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
