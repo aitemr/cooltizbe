01ee276b2766076d2142c0d4c32edbd769e696d3
Islam Temirbek <aitemr16@gmail.com>
2018-12-05 23:32:29 +0600

    fix: remove unnecessary files & rename assets folder

--- app/cooltizbe/Controllers/ScheduleViewController.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-class ScheduleViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-}
--- app/cooltizbe/Coordinators/Coordinator.swift
+++
@@ -1,5 +0,0 @@
-import UIKit
-
-class Coordinator: NSObject {
-
-}
