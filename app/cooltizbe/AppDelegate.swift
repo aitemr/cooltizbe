fe5c39d741b37560388bbcd80981f627defbd772
Islam Temirbek <aitemr16@gmail.com>
2018-12-09 03:31:06 +0600

    fix: reorganize folder structure

--- app/cooltizbe/AppDelegate.swift
+++ app/cooltizbe/AppDelegate.swift
@@ -49,6 +49,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
+        
         return container
     }()
 
