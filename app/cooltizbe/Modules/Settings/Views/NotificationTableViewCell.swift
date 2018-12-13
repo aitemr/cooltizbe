84c0f7d28d21475a95ab738d5ddc1c7088367516
Islam Temirbek <aitemr16@gmail.com>
2018-12-13 23:33:26 +0600

    feat(NotificationTableViewCell): add NotificationTableViewCell

---
+++ app/cooltizbe/Modules/Settings/Views/NotificationTableViewCell.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class NotificationTableViewCell: UITableViewCell {
+
+    // MARK: - Properties
+    
+    @IBOutlet private weak var cellImageView: UIImageView!
+    @IBOutlet private weak var title: UILabel!
+    @IBOutlet private weak var switcher: UISwitch!
+    
+    // MARK: - Lifecycle
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
