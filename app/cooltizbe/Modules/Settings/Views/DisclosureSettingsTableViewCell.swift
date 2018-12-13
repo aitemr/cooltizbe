d0ca48203e71f61f2ba034143c4ab3cfd99dc94e
Islam Temirbek <aitemr16@gmail.com>
2018-12-13 23:16:06 +0600

    fix: remove storyboard & add DisclosureSettingsTableViewCell

---
+++ app/cooltizbe/Modules/Settings/Views/DisclosureSettingsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DisclosureSettingsTableViewCell.swift
+//  cooltizbe
+//
+//  Created by Islam on 12/13/18.
+//  Copyright © 2018 Islam Temirbek. All rights reserved.
+//
+
+import UIKit
+
+class DisclosureSettingsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
