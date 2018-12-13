677cd0757737ce9b46ccaf6165a0686bb3f5bf7b
Islam Temirbek <aitemr16@gmail.com>
2018-12-13 23:41:34 +0600

    feat(SettingsTableViewCell): add cell

---
+++ app/cooltizbe/Modules/Settings/Views/SettingsTableViewCell.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class SettingsTableViewCell: UITableViewCell {
+
+    // MARK: - Properties
+    
+    @IBOutlet private weak var cellImageView: UIImageView!
+    @IBOutlet private weak var title: UILabel!
+    
+    // MARK: - Lifecycle
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
