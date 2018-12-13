153281b903172b607d6c40953d3badae74fd8b48
Islam Temirbek <aitemr16@gmail.com>
2018-12-14 00:00:05 +0600

    wip(SettingsViewController): register cell

--- app/cooltizbe/Modules/Settings/Controllers/SettingsViewController.swift
+++ app/cooltizbe/Modules/Settings/Controllers/SettingsViewController.swift
@@ -10,6 +10,29 @@ class SettingsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        registerTableViewCells()
     }
     
+    // MARK: - Register TableView Cells
+    
+    private func registerTableViewCells() {
+        tableView.dataSource = self
+        
+        let notificationNib = UINib(nibName: String(describing: NotificationTableViewCell.self), bundle: nil)
+        tableView.register(notificationNib,
+                           forCellReuseIdentifier: String(describing: NotificationTableViewCell.self))
+    }
+}
+
+extension SettingsViewController: UITableViewDataSource {
+ 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: String(describing: NotificationTableViewCell.self),
+                                                 for: indexPath) as! NotificationTableViewCell
+    }
 }
