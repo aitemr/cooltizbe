56c8b2860af0a85a6146ec62fb3f9de887d9191c
Islam Temirbek <aitemr16@gmail.com>
2018-12-15 19:04:49 +0600

    wip(SearchViewController): fix text field

--- app/cooltizbe/Modules/Search/Controllers/SearchViewController.swift
+++ app/cooltizbe/Modules/Search/Controllers/SearchViewController.swift
@@ -2,10 +2,46 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    @IBOutlet private weak var searchTextField: UITextField!
+    
     // MARK: View LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureTextField()
+        setNeedsStatusBarAppearanceUpdate()
     }
+    
+    // MARK: - UIStatusBarStyle
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .default
+    } 
 
+    // MARK: - Actions
+    
+    @IBAction func closeButtonDidPress(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - Configure TextField
+    
+    private func configureTextField() {
+        let border = CALayer()
+        let width = CGFloat(1.0)
+        border.borderColor = #colorLiteral(red: 0.8431372549, green: 0.8431372549, blue: 0.8431372549, alpha: 1)
+        border.frame = CGRect(x: 0,
+                              y: searchTextField.frame.size.height - width,
+                              width: searchTextField.frame.size.width,
+                              height: searchTextField.frame.size.height)
+        
+        border.borderWidth = width
+        searchTextField.layer.addSublayer(border)
+        searchTextField.layer.sublayerTransform = CATransform3DMakeTranslation(16, 0, 0)
+        searchTextField.layer.masksToBounds = true
+    }
+    
 }
