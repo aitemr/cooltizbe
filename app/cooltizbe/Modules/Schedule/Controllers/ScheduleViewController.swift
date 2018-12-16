9c6bed6c39b3afc19127c37ef1f91109d9e24c69
Islam Temirbek <aitemr16@gmail.com>
2018-12-16 19:02:16 +0600

    fix(ScheduleViewController): fix status bar color & navigation bar

--- app/cooltizbe/Modules/Schedule/Controllers/ScheduleViewController.swift
+++ app/cooltizbe/Modules/Schedule/Controllers/ScheduleViewController.swift
@@ -10,23 +10,14 @@ class ScheduleViewController: UIViewController {
         configureNavigationBar()
     }
     
-    // MARK: - UIStatusBarStyle
-    
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
-    }
-    
     // MARK: Configure Navigation Bar
     
     private func configureNavigationBar() {
-
-        navigationController?.navigationBar.titleTextAttributes =
-            [NSAttributedString.Key.foregroundColor: UIColor.red]
+//        navigationController?.navigationBar.titleTextAttributes =
+//            [NSAttributedString.Key.foregroundColor: UIColor.red]
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "CSSE-145K", style: .plain, target: self, action: nil)
         
-//        navigationController?.navigationBar.setGradientBackground(colors: [.gradientPrimaryColor,
-//                                                                           .gradientSecondaryColor])
         let searchBarButtonItem = UIBarButtonItem(image: UIImage(named: "schedule_search"),
                                                   style: .plain,
                                                   target: self,
@@ -39,17 +30,17 @@ class ScheduleViewController: UIViewController {
         
         navigationItem.rightBarButtonItems = [settingsBarButtonItem, searchBarButtonItem]
         
-        navigationController?.navigationBar.barTintColor = .white
+        navigationController?.navigationBar.barTintColor = .gradientSecondaryColor
     }
 
     // MARK: Actions
     
     @objc private func searchBarButtonItemDidPress() {
-        present(SearchViewController(), animated: true, completion: nil)
+        present(UINavigationController(rootViewController: SearchViewController()), animated: true, completion: nil)
     }
     
     @objc private func settingsBarButtonItemDidPress() {
-        present(SettingsViewController(), animated: true, completion: nil)
+        present(UINavigationController(rootViewController: SettingsViewController()), animated: true, completion: nil)
     }
     
 }
