829d9c979329b0157a43ee0dbe950fa41ba4f6b0
Islam Temirbek <aitemr16@gmail.com>
2018-12-17 01:40:20 +0600

    feat(ScheduleTableViewCell): add cell

---
+++ app/cooltizbe/Modules/Schedule/Views/ScheduleTableViewCell.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class ScheduleTableViewCell: UITableViewCell {
+
+    // MARK: - Properties
+    
+    @IBOutlet private weak var subjectNameLabel: UILabel!
+    @IBOutlet private weak var teacherNameLabel: UILabel!
+    @IBOutlet private weak var timeLabel: UILabel!
+    @IBOutlet private weak var roomLabel: UILabel!
+    
+}
