b3ea0f162c4eb06ec55eec145c5d3cb4f22b0b88
Islam Temirbek <aitemr16@gmail.com>
2018-12-17 00:54:58 +0600

    feat(ScheduleHeaderView): add xib

--- app/cooltizbe/Modules/Schedule/Views/ScheduleHeaderView.swift
+++ app/cooltizbe/Modules/Schedule/Views/ScheduleHeaderView.swift
@@ -2,4 +2,16 @@ import UIKit
 
 class ScheduleHeaderView: UIView {
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        loadFromNib()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        loadFromNib()
+    }
+    
 }
