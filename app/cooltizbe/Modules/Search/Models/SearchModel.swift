1c67f23e2d31761bd98dfa356fc3be69344ca2b9
Islam Temirbek <aitemr16@gmail.com>
2018-12-18 15:51:42 +0600

    feat(SearchService): update service

--- app/cooltizbe/Modules/Search/Models/SearchModel.swift
+++ app/cooltizbe/Modules/Search/Models/SearchModel.swift
@@ -1,5 +1,13 @@
 import UIKit
 
-class SearchModel: NSObject {
-
+struct Search: Decodable {
+    let id: String
+    let type: String
+    let nameEn: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case type
+        case nameEn = "name_en"
+    }
 }
--- app/cooltizbe/Modules/Search/Services/SearchService.swift
+++ app/cooltizbe/Modules/Search/Services/SearchService.swift
@@ -1,9 +1,26 @@
-//
-//  SearchService.swift
-//  cooltizbe
-//
-//  Created by Islam on 12/18/18.
-//  Copyright © 2018 Islam Temirbek. All rights reserved.
-//
+import Alamofire
+import CodableAlamofire
 
-import Foundation
+struct SearchService {
+    
+    func search(with query: String, completion: @escaping([Search]?) -> Void) {
+        guard let url = URL(string: "http://schedule.iitu.kz/rest/user/search.php?query=\(query)&count=4") else {
+            completion(nil)
+            return
+        }
+
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .secondsSince1970
+
+        Alamofire.request(url).responseDecodableObject(keyPath: "result", decoder: decoder) {
+            (response: DataResponse<[Search]>) in
+            
+            if !response.result.isSuccess {
+                completion(nil)
+                return
+            }
+            
+            completion(response.result.value)
+        }
+    }
+}
