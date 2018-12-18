c3497505285ada29e5ccb0498a89689b2b941035
Islam Temirbek <aitemr16@gmail.com>
2018-12-18 12:00:56 +0600

    feat(ScheduleHeaderView): add outlets & actions

--- app/cooltizbe/Modules/Schedule/Controllers/ScheduleViewController.swift
+++ app/cooltizbe/Modules/Schedule/Controllers/ScheduleViewController.swift
@@ -94,7 +94,7 @@ class ScheduleViewController: UIViewController {
 extension ScheduleViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
--- app/cooltizbe/Modules/Schedule/Views/ScheduleHeaderView.swift
+++ app/cooltizbe/Modules/Schedule/Views/ScheduleHeaderView.swift
@@ -2,6 +2,15 @@ import UIKit
 
 class ScheduleHeaderView: UIView {
 
+    // MARK: - Properties
+    
+    @IBOutlet private weak var mondayButton: UIButton!
+    @IBOutlet private weak var tuesdayButton: UIButton!
+    @IBOutlet private weak var wednesdayButton: UIButton!
+    @IBOutlet private weak var thursdayButton: UIButton!
+    @IBOutlet private weak var fridayButton: UIButton!
+    @IBOutlet private weak var saturdayButton: UIButton!
+    
     // MARK: - Initialization
     
     override init(frame: CGRect) {
@@ -14,6 +23,12 @@ class ScheduleHeaderView: UIView {
         super.init(coder: aDecoder)
         
         loadFromNib()
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func weekDayButtonDidPress(_ sender: UIButton) {
+        
     }
     
 }
